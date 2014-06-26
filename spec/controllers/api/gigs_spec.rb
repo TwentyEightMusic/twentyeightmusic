f80648bedd2cfb6338810b7c40b3a9041c56985e
Justin Kenyon <kenyonj@gmail.com>
2014-06-26 17:00:07 -0400

    basic styling

--- spec/controllers/api/gigs_spec.rb
+++ spec/controllers/api/gigs_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe Api::V1::GigsController do
   context "#create" do
     it "creates a gig from a valid api key request" do
-      api_key = create(:api_key)
+      api_key = build_stubbed(:api_key)
       gig_info = { venue: "The WoodShed", start_time: Time.now }
 
-      ApiKey.stub(:find_by) { api_key }
-      get :create, api_key: api_key, gig_info: gig_info
+      expect(ApiKey).to receive(:find_by).and_return(api_key)
+      post :create, api_key: api_key, gig_info: gig_info
 
       expect(Gig.all.count).to eq 1
     end
