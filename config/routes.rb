39d5a1495b997a04bbc606cf0c26cda1e5448853
Justin Kenyon <kenyonj@gmail.com>
2016-03-06 13:33:52 -0500

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :gigs, only: [:create, :index]
+      resources :gigs, only: [:create]
     end
   end
 end
