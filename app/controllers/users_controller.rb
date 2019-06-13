19765b30129b9bf6834e79ff28bd5d738dd20cea
Justin Kenyon <kenyonj@gmail.com>
2019-06-13 12:59:08 -0400

    do the same for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
+  before_action :require_login, except: [:new, :create]
 
   def new
     @user = User.new
